80907b2b9695928797aea9d7d9b3f685f567f83b
SauceSavior <ethan.steckler259@gmail.com>
2022-09-19 09:57:11 -0500

    python intro - commands

---
+++ PythonMaterial/CSE-Unit3/HelloWorld.py
@@ -0,0 +1,26 @@
+print("Hello World") 
+# is many note
+"""
+    This is how
+    you can make notes
+    in many lines
+"""
+print("Idk just more text")
+input("the put of information")
+#input(messge or quesion to user)
+#variables are data storage 
+name = input("What is your name?")
+#concatenation con-cat-e-nation 
+print ("Hello! "+name)
+#camelCase is lowerFirst upperSecond 
+
+#KANSTANT BARIABLE 
+CONSTANT_VARIABLE = "only for variables that should never change"
+#DONT CAP FIRST LETTER
+# ^WHAT HE SAID
+print("This is a number "+str(5))
+
+print(f"This is a fancy string {5}")
+
+age = input(f"How old is ye {name}?")
+print(f"ye are {age} old HAR HAR HAR!")
